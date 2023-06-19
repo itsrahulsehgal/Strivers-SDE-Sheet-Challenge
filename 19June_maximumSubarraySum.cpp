65f6f94c50bd1c30b45c9ebdd5f9aa7e3bce4fdf
Rahul Sehgal <104090821+itsrahulsehgal@users.noreply.github.com>
2023-06-19 15:53:19 +0530

    Create 19June_maximumSubarraySum.cpp

---
+++ 19June_maximumSubarraySum.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h> 
+long long maxSubarraySum(int arr[], int n)
+{
+   long long sum=0;
+    long long currsum=0;
+    for(int i=0;i<n;i++){
+        if(currsum<0) currsum=0;
+        currsum+=arr[i];
+        sum=max(currsum, sum);
+    }
+    return sum;
+}
