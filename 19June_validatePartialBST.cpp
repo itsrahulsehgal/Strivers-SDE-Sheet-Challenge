c6276f0b4f4b35746fb2b6efb3855146e2bc7a2e
Rahul Sehgal <104090821+itsrahulsehgal@users.noreply.github.com>
2023-06-19 23:28:42 +0530

    Create 19June_validatePartialBST.cpp

---
+++ 19June_validatePartialBST.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h> 
+/*************************************************************
+ 
+    Following is the Binary Tree node structure
+
+    class BinaryTreeNode 
+    {
+    public : 
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+*************************************************************/
+
+bool validateBST(BinaryTreeNode<int> *root) {
+     if(root==NULL) return true;
+    bool flag=true;
+    if(root->left) flag=root->left->data <= root->data;
+    if(root->right) flag= flag && (root->right->data >= root->data);
+    return flag && validateBST(root->left) && validateBST(root->right);
+}
